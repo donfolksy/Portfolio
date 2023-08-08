9c1e30f74004c2f57b399957c22bc0e6753fca48
Afolabi Boluwatife <afolabibolu15@gmail.com>
2023-08-08 13:55:34 +0100

    connect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,14 +13,15 @@ function Header() {
     <div className="wrapper flex justify-between items-center h-screen w-full px-20 lg:justify-center lg:px-6">
       <div className="content lg:text-center">
         <div className="btn py-3">
-          <button  className=' border-2 border-[#b0aaaa] font-bold text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-4 rounded-2xl'>Welcome to my Portfolio</button>
+          <button  className='  text-white text-2xl'>Welcome to my Portfolio</button>
         </div>
         <h1 className='text-white text-5xl font-bold'>Hi! I'm Boluwatife</h1>
         <p className='text-white py-4 max-w-lg '>I'm a Frontend Web Developer</p>
        <a href="https://wa.me/+2349162484365">
-       <button className='  text-white text-2xl'>Let's Connect <i className="fa-solid fa-arrow-right text-lg  p-[2px] "></i> </button>
+       <button  className=' border-2 border-[#b0aaaa] font-bold text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-4 rounded-2xl'>Let's Connect <i className="fa-solid fa-arrow-right text-lg  p-[2px] "></i> </button>
        </a>
       </div>
+      {/*  */}
       <div className="image banner-astronout lg:hidden absolute w-[300px] flex ">
       <img className='w-96 ast-img' src={logo1} alt="" />
     
